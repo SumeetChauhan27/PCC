8a4f7b31f356169c78ea1494912a4cc920657b66
SumeetChauhan27 <146855389+SumeetChauhan27@users.noreply.github.com>
2025-04-22 15:56:49 +0530

    Add files via upload

---
+++ 2.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int arr[100], n, i;
+    clrscr();
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Deleting first element...\n");
+    for(i = 0; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+
+    n--;
+
+    printf("Array after deletion:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    getch();
+}
